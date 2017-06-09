b046e767405660f7ca902dbf091c706a62194aef
Kevin <walchko@users.noreply.github.com>
2017-06-09 13:53:58 -0600

    Create __init__.py

---
+++ nxp/__init__.py
@@ -0,0 +1,7 @@
+from . import FXAS21002.FXAS21002
+from . import FXOS8700.FXOS8700
+from . import version.__verions__
+
+__author__ = 'Kevin J. Walchko'
+__license__ = 'MIT'
+__copyright__ = '2017 Kevin J. Walchko'
