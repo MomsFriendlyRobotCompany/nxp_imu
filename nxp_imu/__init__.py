8bcf9d09234037cf40ba184a1b448b18663f342d
Kevin <walchko@users.noreply.github.com>
2017-06-12 11:24:33 -0600

    update

--- nxp_imu/__init__.py
+++ nxp_imu/__init__.py
@@ -3,7 +3,7 @@ from .FXOS8700 import FXOS8700
 from .I2C import I2C
 from .IMU import IMU
 from .AHRS import AHRS
-# from .version import __version__
+from .version import __version__
 
 __author__ = 'Kevin J. Walchko'
 __license__ = 'MIT'
