1a2de331e95d0c44de19cb2132cf9741c115a04b
walchko <walchko@users.noreply.github.com>
2017-07-22 10:59:57 -0600

    fix docs and refactor

--- nxp_imu/IMU.py
+++ nxp_imu/IMU.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from __future__ import division
 # from math import atan2, sin, cos, pi
 from .FXAS21002 import FXAS21002
 from .FXOS8700 import FXOS8700
+from math import sin, cos, atan2, pi, sqrt, asin
+# from math import radians as deg2rad
+# from math import degrees as rad2deg
 
 
 class IMU(object):
@@ -24,3 +28,40 @@ class IMU(object):
 		accel, mag = self.accel.get()
 		gyro = self.gyro.get()
 		return (accel, mag, gyro)
+
+	def getOrientation(self, accel, mag, deg=True):
+		ax, ay, az = self.normalize(*accel)
+		mx, my, mz = self.normalize(*mag)
+
+		roll = atan2(ay, az)
+		pitch = atan2(-ax, ay*sin(roll)+az*cos(roll))
+
+		heading = atan2(
+			mz*sin(roll) - my*cos(roll),
+			mx*cos(pitch) + my*sin(pitch)*sin(roll) + mz*sin(pitch)*cos(roll)
+		)
+
+		if deg:
+			roll *= 180/pi
+			pitch *= 180/pi
+			heading *= 180/pi
+
+			heading = heading if heading >= 0.0 else 360 + heading
+			heading = heading if heading <= 360 else heading - 360
+		else:
+			heading = heading if heading >= 0.0 else 2*pi + heading
+			heading = heading if heading <= 2*pi else heading - 2*pi
+
+		return (roll, pitch, heading)
+
+	def normalize(self, x, y, z):
+		"""Return a unit vector"""
+		norm = sqrt(x * x + y * y + z * z)
+		if norm > 0.0:
+			inorm = 1/norm
+			x *= inorm
+			y *= inorm
+			z *= inorm
+		else:
+			raise Exception('division by zero: {} {} {}'.format(x, y, z))
+		return (x, y, z)
