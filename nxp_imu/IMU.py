fcee91c0845bdb623c500727612e2f73d829aa05
Kevin <walchko@users.noreply.github.com>
2019-07-07 08:40:23 -0400

    Update IMU.py

--- nxp_imu/IMU.py
+++ nxp_imu/IMU.py
@@ -17,17 +17,53 @@ class IMU(object):
         """
         self.accel = FXOS8700(gs=gs, verbose=verbose)
         self.gyro = FXAS21002(dps=dps, bw=gyro_bw, verbose=verbose)
+        self.bias_a = None
+        self.bias_g = None
+        self.bias_m = None
 
     def __del__(self):
         """
         """
         pass
+    
+    def setBias(self, a, m, g):
+        """
+        Set the bias for accel, mag, and/or gyro. If you do
+        not want to set a bias correction for a sensor, just 
+        pass None.
+        """
+        if a and len(a) == 3:
+            self.bias_a = a
+        
+        if m and len(m) == 3:
+            self.bias_m = m
+            
+        if g and len(g) == 3:
+            self.bias_g = g
+           
 
     def get(self):
         """
+        Returns a reading from all 3 sensors. If a bias correction
+        was set, then the sensor gets corrected.
+        
+        sensor = sensor_raw - bias
         """
         accel, mag = self.accel.get()
         gyro = self.gyro.get()
+        
+        if self.bias_a:
+            a = self.bias_a
+            accel = (accel[0]-a[0],accel[1]-a[1],accel[2]-a[2],)
+            
+        if self.bias_g:
+            g = self.bias_g
+            gyro = (gyro[0]-g[0],gyro[1]-g[1],gyro[2]-g[2],)
+            
+        if self.bias_m:
+            m = self.bias_m
+            mag = (mag[0]-m[0], mag[1]-m[1], mag[2]-m[2],)
+            
         return (accel, mag, gyro)
 
     def getOrientation(self, accel, mag, deg=False):
